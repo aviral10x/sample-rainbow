4f3ba1e26118aff07aac8e48a2acb5f0bef3300d
nlok5923 <nlokhande5923@gmail.com>
2023-05-10 09:34:27 +0530

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, WagmiConfig, createClient } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { BananaWallet } from "@rize-labs/banana-rainbowkit-plugin";
+import { Demo } from "./demo";
 
 function App() {
+  const { chains, provider } = configureChains(
+    [mainnet, polygon, optimism, arbitrum],
+    [publicProvider()]
+  );
+  // const { connectors } = getDefaultWallets({
+  //   appName: "My RainbowKit App",
+  //   chains,
+  // });
+
+  const connectors = connectorsForWallets([
+    {
+      groupName: 'Recommended',
+      wallets: [
+        BananaWallet({
+          chains,
+          connect: {
+            networkId: 80001
+          }
+        }),
+      ]
+    }])
+
+  const wagmiConfig = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WagmiConfig client={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          <Demo />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </div>
   );
 }
